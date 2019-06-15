98e72109a1fddc6a082eb1591ce1097182dc1429
gonzicz <gonzicz83@gmail.com>
2019-06-15 22:08:35 +0200

    stworzenie niwego projektu

---
+++ wypozyczalnia_samochodow/src/test/java/pl/rl/wypozyczalnia_samochodow/WypozyczalniaSamochodowApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.rl.wypozyczalnia_samochodow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WypozyczalniaSamochodowApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
